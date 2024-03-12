0fad009afcfd4733ddfa3eec988cdb40811e2fe6
hardworkingconductor <112430113+hardworkingconductor@users.noreply.github.com>
2024-03-12 16:05:51 +0100

    Update Payload_decoder.js

--- kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
+++ kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
@@ -8,14 +8,14 @@ function decodeUplink(input) {
       4: "button",
     };
     data.event = events[input.fPort];
-    data.temp = (input.bytes[0] << 8) + input.bytes[1];
-   // pen.humitidy = input.byes[2];
-    data.light = (input.bytes[3] << 8) + input.bytes[4];
-    
     data.temperature =
-      (((input.bytes[4] & 0x80 ? input.bytes[4] - 0x100 : input.bytes[4]) << 8) +
-        input.bytes[5]) /
-      100;
+    (((input.bytes[0] & 0x80 ? input.bytes[0] - 0x100 : input.bytes[0]) << 8) +
+      input.bytes[1]) /
+    10;
+    data.humitidy = input.bytes[2];
+    data.light = (input.bytes[3] << 8) + input.bytes[4];
+
+
     var warnings = [];
     if (data.temperature < -10) {
       warnings.push("it's cold");
