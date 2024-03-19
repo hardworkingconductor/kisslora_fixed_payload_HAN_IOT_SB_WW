6726e1440a3d5aa8743fd2fd308c6bb80ae224be
hardworkingconductor <112430113+hardworkingconductor@users.noreply.github.com>
2024-03-19 11:19:59 +0100

    Update Payload_decoder.js

--- kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
+++ kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
@@ -1,18 +1,10 @@
 function decodeUplink(input) {
     var data = {};
-    var pen= {};
-    var events = {
-      1: "setup",
-      2: "interval",
-      3: "motion",
-      4: "button",
-    };
-    data.event = events[input.fPort];
     data.temperature =
     (((input.bytes[0] & 0x80 ? input.bytes[0] - 0x100 : input.bytes[0]) << 8) +
       input.bytes[1]) /
     10;
-    //data.humitidy = (((input.bytes[2] & 0x80 ? input.bytes[2] - 0x100 : input.bytes[2]) << 8) //controleer deze functie
+    data.humitidy = ((input.bytes[2] & 0x80 ? input.bytes[2] - 0x100 : input.bytes[2]) << 8); //controleer deze functie
     data.light = (input.bytes[3] << 8) + input.bytes[4];
     data.rotpos = input.bytes[5];
     data.acX =  (((input.bytes[6] & 0x80 ? input.bytes[6] - 0x100 : input.bytes[6]) << 8) +
@@ -24,14 +16,20 @@ function decodeUplink(input) {
     data.acZ =  (((input.bytes[10] & 0x80 ? input.bytes[10] - 0x100 : input.bytes[10]) << 8) +
       input.bytes[11]) /
     1000;
+    //dit werkt niet, fix het
     data.analogIN =
-    (((input.bytes[12] & 0x80 ? input.bytes[12] - 0x100 : input.bytes[12) << 8) +
+    (((input.bytes[12] & 0x80 ? input.bytes[12] - 0x100 : input.bytes[12]) << 8) +
       input.bytes[13]) /
     100;
-    //dit werkt niet, fix het
+    data.presence = input.bytes[14];
+    data.analogOUT =
+    (((input.bytes[15] & 0x80 ? input.bytes[15] - 0x100 : input.bytes[15]) << 8) +
+      input.bytes[16]) /
+    100;
+    
     
 
-
+//}
     var warnings = [];
     if (data.temperature < -10) {
       warnings.push("it's cold");
@@ -41,13 +39,10 @@ function decodeUplink(input) {
       warnings: warnings,
     };
   }
-  
+
   function normalizeUplink(input) {
     return {
       data: {
-        air: {
-          temperature: input.data.temperature,
-        },
       },
     };
   }
