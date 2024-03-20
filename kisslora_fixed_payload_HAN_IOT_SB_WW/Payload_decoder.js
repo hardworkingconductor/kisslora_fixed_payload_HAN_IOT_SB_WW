b96377196b73e174f63a9a020e6b6d751fbbb806
hardworkingconductor <112430113+hardworkingconductor@users.noreply.github.com>
2024-03-20 21:35:37 +0100

    Update Payload_decoder.js

--- kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
+++ kisslora_fixed_payload_HAN_IOT_SB_WW/Payload_decoder.js
@@ -1,14 +1,26 @@
+/*
+To use this custom payload decoder go to payload formatters in your
+things network console.
+Then click on the uplink and select custom javascript.
+paste this code there and save it.
+*/
+
 function decodeUplink(input) {
     var data = {};
-
+    
+    //checks if the data is the short message or the long message
     if(input.bytes.length === 2){
+    //short message
     data.presence = input.bytes[0];
     data.digIN = input.bytes[1];
+      
     } else{
+    //long message
     data.temperature =
     (((input.bytes[0] & 0x80 ? input.bytes[0] - 0x100 : input.bytes[0]) << 8) +
       input.bytes[1]) /
     10;
+    //calculates the unsigned value back into a signed value
     data.humitidy = ((input.bytes[2] & 0x80 ? input.bytes[2] - 0x100 : input.bytes[2]) << 8); //controleer deze functie
     data.light = (input.bytes[3] << 8) + input.bytes[4];
     data.rotpos = input.bytes[5];
@@ -21,7 +33,6 @@ function decodeUplink(input) {
     data.acZ =  (((input.bytes[10] & 0x80 ? input.bytes[10] - 0x100 : input.bytes[10]) << 8) +
       input.bytes[11]) /
     1000;
-    //dit werkt niet, fix het
     data.analogIN =
     (((input.bytes[12] & 0x80 ? input.bytes[12] - 0x100 : input.bytes[12]) << 8) +
       input.bytes[13]) /
@@ -31,16 +42,10 @@ function decodeUplink(input) {
     (((input.bytes[15] & 0x80 ? input.bytes[15] - 0x100 : input.bytes[15]) << 8) +
       input.bytes[16]) /
     100;
-    
-    }
-    
-    var warnings = [];
-    if (data.temperature < -10) {
-      warnings.push("it's cold");
-    }
+    }   
+
     return {
       data: data,
-      warnings: warnings,
     };
   }
 
