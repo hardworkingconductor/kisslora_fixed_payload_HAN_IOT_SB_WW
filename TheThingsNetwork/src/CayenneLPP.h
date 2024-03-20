3a771534e736788f92a0ae3f8078403b0333daca
hardworkingconductor <112430113+hardworkingconductor@users.noreply.github.com>
2024-03-20 21:37:42 +0100

    Update CayenneLPP.h

--- TheThingsNetwork/src/CayenneLPP.h
+++ TheThingsNetwork/src/CayenneLPP.h
@@ -3,6 +3,8 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
+
+
 #ifndef _CAYENNE_LPP_H_
 #define _CAYENNE_LPP_H_
 
@@ -11,6 +13,8 @@
 //LPP_BATTERY = // TODO Unsupported in IPSO Smart Object
 //LPP_PROXIMITY = // TODO Unsupported in IPSO Smart Object
 
+
+/// denotes how large the values are and what types.
 //#define LPP_DIGITAL_INPUT 0         // 1 byte
 //#define LPP_DIGITAL_OUTPUT 1        // 1 byte
 //#define LPP_ANALOG_INPUT 2          // 2 bytes, 0.01 signed
@@ -26,13 +30,13 @@
 
 // Data ID + Data Type + Data Size
 
-//#define LPP_DIGITAL_SIZE 3				//2 byes unsigned
+//#define LPP_DIGITAL_SIZE 3		     //2 byes unsigned
 //#define LPP_ANALOG_INPUT_SIZE 4        // 2 bytes, 0.01 signed
 //#define LPP_ANALOG_OUTPUT_SIZE 4       // 2 bytes, 0.01 signed
-//#define LPP_ANALOG_SIZE 4			   //
+//#define LPP_ANALOG_SIZE 4			     //
 //#define LPP_LUMINOSITY_SIZE 4          // 2 bytes, 1 lux unsigned
 //#define LPP_PRESENCE_SIZE 3            // 1 byte, 1
-//define LPP_TEMPERATURE_SIZE 4         // 2 bytes, 0.1°C signed
+//#define LPP_TEMPERATURE_SIZE 4          // 2 bytes, 0.1°C signed
 //#define LPP_RELATIVE_HUMIDITY_SIZE 3   // 1 byte, 0.5% unsigned
 //#define LPP_ACCELEROMETER_SIZE 8       // 2 bytes per axis, 0.001G
 //#define LPP_BAROMETRIC_PRESSURE_SIZE 4 // 2 bytes 0.1 hPa Unsigned
@@ -45,30 +49,48 @@ public:
   CayenneLPP(uint8_t size);
   ~CayenneLPP();
 
+///	\param resets the cursor to 0 so that all the elements can be rewritten again
   void reset(void);
+///	\param checks and returns the length of the array
+/// \return length of the array
   uint8_t getSize(void);
+  
+///	\param checks and returns buffer values
+/// \return buffer values
   uint8_t *getBuffer(void);
+  
+/// \param copies block of memory
   uint8_t copy(uint8_t *buffer);
 
-  
+///	\param adds a digital sensor value to the array for transmitting
+/// \return uses an uint8_t value
   uint8_t addDigital(uint8_t value);
-
+///	\param adds a analog sensor value to the array for transmitting
+/// \return uses an uint8_t value
   uint8_t addAnalog( uint8_t value);
-
+///	\param adds a luminosity sensor value to the array for transmitting
+/// \return uses an uint16_t value
   uint8_t addLuminosity( uint16_t lux);
+///	\param adds a precense sensor value to the array for transmitting
+/// \return uses an uint8_t value
   uint8_t addPresence( uint8_t value);
+///	\param adds a temperature sensor value to the array for transmitting
+/// \return uses an float value
   uint8_t addTemperature( float celsius);
+///	\param adds a humidity sensor value to the array for transmitting
+/// \return uses an float value
   uint8_t addRelativeHumidity(float rh);
+///	\param adds accelermotere sensor values to the array for transmitting
+/// \return uses 3 float values
   uint8_t addAccelerometer(float x, float y, float z);
 
 
 	
 
 private:
-  uint8_t *buffer;
-  uint8_t maxsize;
-  uint8_t cursor;
-  //uint8_t smallpayloadesambled[2];
+  uint8_t *buffer; ///< allocsates bytes of memory to storing variables
+  uint8_t maxsize; ///< maximum length the array can be. defined in the ino file
+  uint8_t cursor;  ///< pointer to the locations in the array
 };
 
 #endif
